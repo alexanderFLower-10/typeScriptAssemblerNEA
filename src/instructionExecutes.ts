947cdcf02a2ccc6c2faff70ae8a7782ce3f03e0a
alexanderFLower-10 <alexanderfowlerwork@gmail.com>
2025-06-10 11:53:38 +0100

    newstuff

--- src/instructionExecutes.ts
+++ src/instructionExecutes.ts
@@ -1,5 +1,5 @@
 import { ARMEmulator } from "./ARMEmulator";
-import { Instruction, ThreeParameterInstruction } from "./parameterClassDefinitions";
+import { Instruction, ThreeParameterInstruction, TwoParameterInstruction } from "./parameterClassDefinitions";
 
 export abstract class ThreeParameterExecutes{
     abstract Execute(ARM : ARMEmulator, Parameters : ThreeParameterInstruction) : void;
@@ -67,6 +67,49 @@ export class EOR extends ThreeParameterExecutes{
 export class LSL extends ThreeParameterExecutes{
     constructor(){super();};
     Execute(ARM : ARMEmulator, Parameters : ThreeParameterInstruction) : void{
-        let result : number = Parameters.getRn
+        let result : number = Parameters.getRn() * Math.pow(2, Parameters.getOperand2());
+        ARM.setRegister(Parameters.getRd(), result);
+
+    }
+}
+export class LSR extends ThreeParameterExecutes{
+    constructor(){super();};
+    Execute(ARM : ARMEmulator, Parameters : ThreeParameterInstruction) : void{
+        let result : number = Parameters.getRn() * Math.pow(2,1/ Parameters.getOperand2());
+        ARM.setRegister(Parameters.getRd(), result);
+        
+    }
+}
+
+export abstract class TwoParameterExecutes{
+    abstract Execute(ARM : ARMEmulator, Parameters : TwoParameterInstruction) : void;
+}
+export class MOV extends TwoParameterExecutes{
+    constructor(){super();}
+    Execute(ARM : ARMEmulator, Parameters : TwoParameterInstruction) : void{
+        ARM.setRegister(Parameters.getRd(), Parameters.getOperand2());
+    }
+}
+export class MVN extends TwoParameterExecutes{
+    constructor(){super();}
+    Execute(ARM : ARMEmulator, Parameters : TwoParameterInstruction) : void{
+        let initial : string = Instruction.toBinary(Parameters.getOperand2());
+        let result = "";
+        for(let i = 0; i < initial.length; i++){
+            result+= initial.charAt(i) == '1' ? '0' : '1';
+        }
+        ARM.setRegister(Parameters.getRd(), Instruction.toDenary(result));
+    }
+}
+export class LDR extends TwoParameterExecutes{
+    constructor(){super();}
+    Execute(ARM : ARMEmulator, Parameters : TwoParameterInstruction) : void{
+        ARM.setRegister(Parameters.getRd(), ARM.getMemory(Parameters.getOperand2()));
+    }
+}
+export class STR extends TwoParameterExecutes{
+    constructor(){super();}
+    Execute(ARM : ARMEmulator, Parameters : TwoParameterInstruction) : void{
+        ARM.setMemory(Parameters.getOperand2(), para)
     }
 }
